bda475812d7b2652c49d04991eb00cabbe8d796e
Özgür Deli <oezguer.deli@gmail.com>
2025-06-07 16:05:15 +0200

    changing content to my details and information

--- content/main.ts
+++ content/main.ts
@@ -5,16 +5,20 @@ export const attributes = {
       link: '/#portfolio',
       text: 'Portfolio',
     },
-    heading: "I'm Paco Cu, and I create",
+    heading: "I'm Özgür Deli, and I create",
     image: '/img/hero.jpeg',
     secondButton: { icon: 'whatsapp', link: '/#about', text: 'About me' },
     social: {
       facebook: { link: 'https://facebook.com', show: false },
-      instagram: { link: 'https://instagram.com', show: true },
-      linkedin: { link: 'https://linkedin.com', show: true },
+      instagram: { link: 'https://www.instagram.com/wedev_it/', show: true },
+      linkedin: {
+        link: 'https://www.linkedin.com/in/%C3%B6zg%C3%BCr-deli/',
+        show: true,
+      },
     },
     typed: [
       { text: 'fast websites.' },
+      { text: 'awesome applications.' },
       { text: 'beautiful logos.' },
       { text: 'great experiences.' },
     ],
@@ -22,9 +26,19 @@ export const attributes = {
   about: {
     heading: 'Who am I?',
     image: '/img/about.jpeg',
-    text: 'Özgür Deli.\n',
+    text: 'I am a passionate Software Developer and experienced Software Tester, specialized in both manual and automated testing, including test management.With a strong focus on quality and precision, I work across industries such as: Lottery systems, SaaS platforms, Online gambling & gaming Insurance & financial services Banking solutions. I combine deep technical knowledge with a strategic mindset to ensure software is not only functional — but reliable, secure, and ready for real-world use.\n',
   },
   features: [
+    {
+      icon: 'xml',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel dapibus mi.\n',
+      title: 'Web Development',
+    },
+    {
+      icon: 'xml',
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel dapibus mi.\n',
+      title: 'Software Testing',
+    },
     {
       icon: 'monitor-screenshot',
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel dapibus mi.\n',
@@ -35,49 +49,14 @@ export const attributes = {
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel dapibus mi.\n',
       title: 'Logo Design',
     },
-    {
-      icon: 'xml',
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel dapibus mi.\n',
-      title: 'Web Development',
-    },
   ],
   portfolio: {
     projects: [
       {
-        btnLink: 'https://behance.com',
-        btnText: 'View on Behance',
+        btnLink: 'https://wedev-it.at',
+        btnText: 'Coming Soon',
         image: '/img/project-1.jpeg',
-        title: 'ShopoShopy',
-      },
-      {
-        btnLink: 'https://behance.com',
-        btnText: 'View on Behance',
-        image: '/img/project-2.jpeg',
-        title: 'LogoLogy',
-      },
-      {
-        btnLink: 'https://behance.com',
-        btnText: 'View on Behance',
-        image: '/img/project-3.jpeg',
-        title: 'SitoSity',
-      },
-      {
-        btnLink: 'https://behance.com',
-        btnText: 'View on Behance',
-        image: '/img/project-4.jpeg',
-        title: 'DrawoDrawy',
-      },
-      {
-        btnLink: 'https://behance.com',
-        btnText: 'View on Behance',
-        image: '/img/project-5.jpeg',
-        title: 'PagoPagy',
-      },
-      {
-        btnLink: 'https://behance.com',
-        btnText: 'View on Behance',
-        image: '/img/project-6.jpeg',
-        title: 'UIoUXy',
+        title: 'Coming Soon',
       },
     ],
     title: 'My work',
